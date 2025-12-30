e2a90a6f73dc192ca1457d95c821e0867b7f48e9
chandankumar811 <wwwck811@gmail.com>
2025-12-30 08:40:10 +0000

    Add google search console verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,16 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('http://localhost:3000'),
+  metadataBase: new URL("http://localhost:3000"),
   title: {
     default: "DevFixPro – Fix Real Backend & Frontend Errors",
     template: "%s | DevFixPro",
   },
   description:
     "Fix real NestJS, Node.js, React, Next.js, MongoDB errors with production-ready solutions.",
+  verification: {
+    google: "XtdqrTFUvoeF70ev72xJRYt6zspcZ3MY1MIlRMiLeQg",
+  },
 };
 
 export default function RootLayout({
